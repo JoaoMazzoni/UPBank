43aac78cfc6cc2c2497e34ab86859380c8830151
Othavio Augusto <othavioact@gmail.com>
2024-06-23 18:30:09 -0300

    add: classe generica para consumo de api

---
+++ EmployeeAPI/ApiConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace EmployeeAPI
+{
+    public class ApiConsumer<T>
+    {
+        private HttpClient _httpClient;
+        private string _baseUrl;
+        
+        public ApiConsumer(string baseUrl)
+        {
+            _httpClient = new HttpClient();
+            _baseUrl = baseUrl;
+        }
+
+        public T Get(string endpoint)
+        {
+            var response = _httpClient.GetAsync(endpoint).Result;
+            var json = response.Content.ReadAsStringAsync().Result;
+            T objectReturn = JsonConvert.DeserializeObject<T>(json);
+            return objectReturn;
+        }
+
+        public T Post(string endpoint, T obj)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = _httpClient.PostAsJsonAsync(endpoint, content).Result;
+
+            string strResponse = response.Content.ReadAsStringAsync().Result;
+
+            T objReturn = JsonConvert.DeserializeObject<T>(strResponse);
+            return objReturn;
+        }
+    }
+}
