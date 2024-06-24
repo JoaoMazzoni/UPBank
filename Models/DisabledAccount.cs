03381d609da19dea6ec77ddfcac3b876ebadc966
EnzoHenrico <enzo.mascia1@gmail.com>
2024-06-24 12:54:41 -0300

    add: DTOs referentes as requisiçoes de servicos

---
+++ Models/DisabledAccount.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models;
+
+public class DisabledAccount
+{
+    [Key] public string Number { get; set; }
+    public string SavingsAccountNumber { get; set; }
+    public string AgencyNumber { get; set; }
+    public string MainClientId { get; set; }
+    public string? SecondaryClientId { get; set; }
+    public bool Restriction { get; set; }
+    public CreditCard? CreditCard { get; set; }
+    public double SpecialLimit { get; set; }
+    public AccountProfile Profile { get; set; }
+    public DateTime Date { get; set; }
+    public double Balance { get; set; }
+}
