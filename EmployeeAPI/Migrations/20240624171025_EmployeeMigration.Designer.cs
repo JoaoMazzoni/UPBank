aa481786cf73a6299df17d4e998df5fb427d8f32
Othavio Augusto <othavioact@gmail.com>
2024-06-24 14:17:04 -0300

    Adequando Dbcontext e migrations para conter DeletedEmployee

--- EmployeeAPI/Migrations/20240624171025_EmployeeMigration.Designer.cs
+++ EmployeeAPI/Migrations/20240624171025_EmployeeMigration.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeAPI.Migrations
 {
     [DbContext(typeof(EmployeeAPIContext))]
-    [Migration("20240623201818_EmployeeMigration")]
+    [Migration("20240624171025_EmployeeMigration")]
     partial class EmployeeMigration
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -22,6 +22,49 @@ namespace EmployeeAPI.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
+
+            modelBuilder.Entity("Models.CopyClasses.DeletedEmployee", b =>
+                {
+                    b.Property<string>("Document")
+                        .HasColumnType("nvarchar(450)");
+
+                    b.Property<string>("AddressId")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("BirthDate")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Email")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Gender")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("Manager")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Phone")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<int>("Register")
+                        .HasColumnType("int");
+
+                    b.Property<double>("Salary")
+                        .HasColumnType("float");
+
+                    b.HasKey("Document");
+
+                    b.ToTable("DeletedEmployee");
+                });
 
             modelBuilder.Entity("Models.Employee", b =>
                 {
