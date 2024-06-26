acf90dfc1a9d4523a21f5d45827fab3c9bdb7974
JoaoMazzoni <joao.mazzoni@5by5.com>
2024-06-26 11:30:05 -0300

    Requisição para a API Employee para verificação no cadastro de clientes.

--- Models/CopyClasses/AccountRequest.cs
+++ Models/CopyClasses/AccountRequest.cs
@@ -21,5 +21,59 @@ namespace Models.CopyClasses
         
         }
 
+
+        public Customer MoveAccountRequest(AccountRequest accountRequest)
+        {
+            var customer = new Customer
+            {
+                Document = accountRequest.Document,
+                Name = accountRequest.Name,
+                BirthDate = accountRequest.BirthDate,
+                Gender = accountRequest.Gender,
+                Salary = accountRequest.Salary,
+                Email = accountRequest.Email,
+                Phone = accountRequest.Phone,
+                Address = accountRequest.Address,
+                AddressId = accountRequest.AddressId
+            };
+
+            return customer;
+        }
+
+        public AccountRequest ToAccountRequest(RemovedCustomer removed)
+        {
+            var accountRequest = new AccountRequest
+            {
+                Document = removed.Document,
+                Name = removed.Name,
+                BirthDate = removed.BirthDate,
+                Gender = removed.Gender,
+                Salary = removed.Salary,
+                Email = removed.Email,
+                Phone = removed.Phone,
+                Address = removed.Address,
+                AddressId = removed.AddressId
+            };
+
+            return accountRequest;
+        }
+
+        public AccountRequest EmployeeToCustomer(Employee employee)
+        {
+            var customer = new AccountRequest
+            {
+                Document = employee.Document,
+                Name = employee.Name,
+                BirthDate = employee.BirthDate,
+                Gender = employee.Gender,
+                Salary = employee.Salary,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Address = employee.Address,
+                AddressId = employee.AddressId
+            };
+            return customer;
+        }
+
     }
 }
